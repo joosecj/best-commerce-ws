964f51e16b6365b4c3c52226630456206973f1e9
Jose Carlos <josecarloscjj@gmail.com>
2023-04-08 17:58:42 -0300

    :sparkles: feat: Created class CategoryProductService. Created methods findAll and insert new Category

---
+++ backend/src/main/java/com/github/bestcommerce/services/CategoryProductService.java
@@ -0,0 +1,35 @@
+package com.github.bestcommerce.services;
+
+import com.github.bestcommerce.dtos.v1.CategoryDTO;
+import com.github.bestcommerce.entities.Category;
+import com.github.bestcommerce.entities.CategoryProduct;
+import com.github.bestcommerce.repositories.CategoryRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryProductService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryProductService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Transactional
+    public Page<CategoryDTO> findAll(Pageable pageable) {
+        Page<Category> categories = categoryRepository.findAll(pageable);
+        return categories.map(CategoryDTO::new);
+    }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO categoryDTO) {
+        Category categoryProduct = new CategoryProduct();
+        categoryProduct.setName(categoryDTO.getName());
+        categoryProduct.setDescription(categoryDTO.getDescription());
+        categoryProduct = categoryRepository.save(categoryProduct);
+        return new CategoryDTO(categoryProduct);
+    }
+
+}
