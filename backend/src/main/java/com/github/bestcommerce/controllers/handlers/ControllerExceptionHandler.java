b3d04bf113abb6f95579ae4e2a7411ef2df80283
Jose Carlos <josecarloscjj@gmail.com>
2023-04-08 20:41:27 -0300

    :goal_net: feat: Database error handling

--- backend/src/main/java/com/github/bestcommerce/controllers/handlers/ControllerExceptionHandler.java
+++ backend/src/main/java/com/github/bestcommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.github.bestcommerce.controllers.handlers;
 
 import com.github.bestcommerce.dtos.handlers.CustomError;
+import com.github.bestcommerce.services.exceptions.DataBaseException;
 import com.github.bestcommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,13 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError error = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<CustomError> dataBase(DataBaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError error = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(error);
     }
--- backend/src/main/java/com/github/bestcommerce/services/CategoryProductService.java
+++ backend/src/main/java/com/github/bestcommerce/services/CategoryProductService.java
@@ -4,8 +4,11 @@ import com.github.bestcommerce.dtos.v1.CategoryDTO;
 import com.github.bestcommerce.entities.Category;
 import com.github.bestcommerce.entities.CategoryProduct;
 import com.github.bestcommerce.repositories.CategoryRepository;
+import com.github.bestcommerce.services.exceptions.DataBaseException;
 import com.github.bestcommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -57,7 +60,14 @@ public class CategoryProductService {
     }
     @Transactional(propagation = Propagation.SUPPORTS)
     public void delete(UUID id) {
-        categoryRepository.deleteById(id);
+        try {
+            categoryRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Category not found");
+        } catch (DataIntegrityViolationException e ) {
+            throw new DataBaseException("Referential integrity failure");
+        }
+
     }
 
 
