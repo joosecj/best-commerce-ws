a9e93d4bfd4b3d9cc7ffb80df651cfd57117e09d
Jose Carlos <josecarloscjj@gmail.com>
2023-04-11 16:57:35 -0300

    :test_tube: test: created test Insert

--- backend/src/test/java/com/github/bestcommerce/services/CategoryProductServiceTests.java
+++ backend/src/test/java/com/github/bestcommerce/services/CategoryProductServiceTests.java
@@ -6,6 +6,7 @@ import com.github.bestcommerce.repositories.CategoryRepository;
 import com.github.bestcommerce.services.exceptions.DataBaseException;
 import com.github.bestcommerce.services.exceptions.ResourceNotFoundException;
 import com.github.bestcommerce.tests.Factory;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -37,42 +38,54 @@ class CategoryProductServiceTests {
     private UUID existingId;
     private UUID nonExistingId;
     private UUID dependentId;
-    private String nonExistingCategory;
     private String existingCategoryNameProductType;
     private String existingCategorNameStoreType;
     private Category category;
     private CategoryDTO categoryDTO;
     private PageImpl<Category> page;
 
+
     @BeforeEach
     void setUp() {
         existingCategoryNameProductType = "PRODUCT";
+        existingCategorNameStoreType = "STORE";
         category = Factory.createCategory(existingCategoryNameProductType);
         categoryDTO = Factory.createCategoryDTO();
         existingId = UUID.fromString("046acaaf-1be6-44b4-b12e-43f51753823e");
         nonExistingId = UUID.randomUUID();
         dependentId = UUID.randomUUID();
-        existingCategoryNameProductType = "TELEVISÃO";
-        existingCategorNameStoreType = "TECNOLOGIA";
         page = new PageImpl<>(List.of(category));
 
         when(categoryRepository.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
 
+        when(categoryRepository.save(ArgumentMatchers.any())).thenReturn(category);
+
+
+        when(categoryRepository.getReferenceById(existingId)).thenReturn(category);
+        when(categoryRepository.getReferenceById(nonExistingId)).thenThrow(EntityNotFoundException.class);
 
         doNothing().when(categoryRepository).deleteById(existingId);
         doThrow(EmptyResultDataAccessException.class).when(categoryRepository).deleteById(nonExistingId);
         doThrow(DataIntegrityViolationException.class).when(categoryRepository).deleteById(dependentId);
-
-
     }
 
     @Test
     void findAllPagedShouldReturnPage() {
         Pageable pageable = PageRequest.of(0, 10);
         Page<CategoryDTO> result = categoryProductService.findAll(pageable);
-
         Assertions.assertNotNull(result);
         Mockito.verify(categoryRepository, times(1)).findAll(pageable);
+    }
+
+    @Test
+    void saveShouldProductReturnCategoryDTO() {
+        CategoryDTO result = categoryProductService.insert(categoryDTO);
+        Assertions.assertNotNull(result);
+    }
+    @Test
+    void updatedShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+        Assertions.assertThrows(ResourceNotFoundException.class,
+                () -> categoryProductService.update(nonExistingId, categoryDTO));
     }
 
     @Test
