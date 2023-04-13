376586398b1bbbdf2bd315e37a428eb2f0a679f5
Jose Carlos <josecarloscjj@gmail.com>
2023-04-13 01:39:06 -0300

    :sparkles: feat: config Swagger Controller

--- backend/src/main/java/com/github/bestcommerce/controllers/CategoryController.java
+++ backend/src/main/java/com/github/bestcommerce/controllers/CategoryController.java
@@ -2,6 +2,12 @@ package com.github.bestcommerce.controllers;
 
 import com.github.bestcommerce.dtos.v1.CategoryDTO;
 import com.github.bestcommerce.services.CategoryProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,11 +15,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.lang.model.type.UnionType;
 import java.net.URI;
 import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/api/v1/categories")
+@Tag(name = "Categories", description = "Endpoints for Managing Category")
 public class CategoryController {
     private final CategoryProductService categoryProductService;
 
@@ -22,19 +30,68 @@ public class CategoryController {
     }
 
     @GetMapping
+    @Operation(summary = "Finds all Categories", description = "Finds all Categories",
+            tags = {"Categories"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = CategoryDTO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) {
         Page<CategoryDTO> dto = categoryProductService.findAll(pageable);
         return ResponseEntity.ok(dto);
     }
 
     @GetMapping(value = "/search")
+    @Operation(summary = "Finds all Categories Search Name", description = "Finds all Categories Search Name",
+            tags = {"Categories"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = CategoryDTO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<Page<CategoryDTO>> searchByName(@RequestParam(name = "name", defaultValue = "") String name,
-                                                    Pageable pageable) {
+                                                          Pageable pageable) {
         Page<CategoryDTO> dto = categoryProductService.searchByName(name, pageable);
         return ResponseEntity.ok(dto);
     }
 
     @PostMapping(value = "/create")
+    @Operation(summary = "Create Category", description = "Create Category and Type Product or Store",
+            tags = {"Categories"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = CategoryDTO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+                    @ApiResponse(description = "Conflict", responseCode = "409", content = @Content),
+            }
+    )
     public ResponseEntity<CategoryDTO> insert(@Valid @RequestBody CategoryDTO dto) {
         dto = categoryProductService.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
@@ -43,12 +100,45 @@ public class CategoryController {
     }
 
     @PutMapping(value = "/{id}")
+    @Operation(summary = "Update Category By Id", description = "Update Category By Id",
+            tags = {"Categories"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = CategoryDTO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+                    @ApiResponse(description = "Conflict", responseCode = "409", content = @Content),
+            }
+    )
     public ResponseEntity<CategoryDTO> update(@PathVariable UUID id, @Valid @RequestBody CategoryDTO dto) {
         dto = categoryProductService.update(id, dto);
         return ResponseEntity.ok(dto);
     }
 
     @DeleteMapping(value = "/{id}")
+    @Operation(summary = "Delete By Id", description = "Delete By Id",
+            tags = {"Categories"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = UnionType.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<Void> delete(@PathVariable UUID id) {
         categoryProductService.delete(id);
         return ResponseEntity.noContent().build();
