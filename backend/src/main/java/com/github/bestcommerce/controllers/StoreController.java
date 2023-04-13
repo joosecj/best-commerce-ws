07411637b1d98d5349159c30667ced568aef087b
Jose Carlos <josecarloscjj@gmail.com>
2023-04-13 16:22:57 -0300

    :sparkles: feat: config swagger Store Controller

--- backend/src/main/java/com/github/bestcommerce/controllers/StoreController.java
+++ backend/src/main/java/com/github/bestcommerce/controllers/StoreController.java
@@ -2,6 +2,12 @@ package com.github.bestcommerce.controllers;
 
 import com.github.bestcommerce.dtos.v1.StoreDTO;
 import com.github.bestcommerce.services.StoreService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,11 +15,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.lang.model.type.UnionType;
 import java.net.URI;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(value = "/api/v1/store")
+@RequestMapping(value = "/api/v1/stores")
+@Tag(name = "Stores", description = "Endpoints for Managing Stores")
 public class StoreController {
 
     private final StoreService storeService;
@@ -23,17 +31,66 @@ public class StoreController {
     }
 
     @GetMapping(value = "/{id}")
+    @Operation(summary = "FindById Store", description = "FindById Store",
+            tags = {"Stores"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = StoreDTO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<StoreDTO> findById(@PathVariable UUID id) {
         StoreDTO dto = storeService.findById(id);
         return ResponseEntity.ok().body(dto);
     }
+
     @GetMapping
+    @Operation(summary = "Find All Stores", description = "Find All Stores",
+            tags = {"Stores"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = StoreDTO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<Page<StoreDTO>> findAll(Pageable pageable) {
         Page<StoreDTO> dto = storeService.findAll(pageable);
         return ResponseEntity.ok(dto);
     }
 
     @PostMapping(value = "/create")
+    @Operation(summary = "Create Store", description = "Create Store",
+            tags = {"Stores"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = StoreDTO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<StoreDTO> insert(@Valid @RequestBody StoreDTO dto) {
         dto = storeService.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
@@ -42,12 +99,45 @@ public class StoreController {
     }
 
     @PutMapping(value = "/{id}")
+    @Operation(summary = "Create Store", description = "Create Store",
+            tags = {"Stores"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = StoreDTO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+                    @ApiResponse(description = "Conflict", responseCode = "409", content = @Content),
+            }
+    )
     public ResponseEntity<StoreDTO> update(@PathVariable UUID id, @Valid @RequestBody StoreDTO dto) {
         dto = storeService.update(id, dto);
         return ResponseEntity.ok(dto);
     }
 
     @DeleteMapping(value = "/{id}")
+    @Operation(summary = "Create Store", description = "Create Store",
+            tags = {"Stores"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = UnionType.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<Void> delete(@PathVariable UUID id) {
         storeService.delete(id);
         return ResponseEntity.noContent().build();
