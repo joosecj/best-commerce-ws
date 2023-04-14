4ee9ec2f989e12c0c91c09d8dcc7065b3784f4b3
Jose Carlos <josecarloscjj@gmail.com>
2023-04-13 23:49:34 -0300

    :sparkles: feat: UserRepository

--- backend/src/main/java/com/github/bestcommerce/repositories/UserRepository.java
+++ backend/src/main/java/com/github/bestcommerce/repositories/UserRepository.java
@@ -2,9 +2,14 @@ package com.github.bestcommerce.repositories;
 
 import com.github.bestcommerce.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-
+    @Query("SELECT u " +
+            "FROM User u " +
+            "WHERE u.email =:email")
+    User findByUserEmail(@Param("email") String email);
 }
